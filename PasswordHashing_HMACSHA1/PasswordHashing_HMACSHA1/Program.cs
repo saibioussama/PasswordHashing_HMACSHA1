fe4cda66487ac1d199ee698b5623e87e73cf5a30
Oussama Saibi <saibioussama@outlook.fr>
2018-01-23 19:23:39 +0100

    init

---
+++ PasswordHashing_HMACSHA1/PasswordHashing_HMACSHA1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Security.Cryptography;
+
+namespace PasswordHashing_HMACSHA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a password: ");
+            string password = Console.ReadLine();
+
+            // generate a 128-bit salt using a secure PRNG
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 10000,
+                numBytesRequested: 512 / 8));
+            Console.WriteLine($"Hashed: {hashed}");
+            Console.ReadKey();
+        }
+    }
+}
